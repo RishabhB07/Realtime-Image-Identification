24610602ca65e190109e51cc8a54e5528ef12cde
RishabhB07 <73791841+RishabhB07@users.noreply.github.com>
2021-07-22 17:20:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 function setup(){
     canvas=createCanvas(250, 250);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/2YYzN8w1n/model.json', modelLoaded);
 }
 function draw(){
-
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+console.log('ml5 version', ml5.version);
+function modelLoaded(){
+    console.log('model loaded!');
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("object_result_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
 }
